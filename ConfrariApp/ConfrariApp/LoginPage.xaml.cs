e4417de448231f81d5aac5093b96b4be51ee4b9b
Luiz Lucas <cluizlucas@gmail.com>
2019-09-30 16:26:57 -0300

    Criação da Tela do Menu Principal, realização de autenticação (Provisória) do Login e Senha. E validação de campos em branco.

---
+++ ConfrariApp/ConfrariApp/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConfrariApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnEntrar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(entryLogin.Text) ||
+               (string.IsNullOrEmpty(entrySenha.Text)))
+            {
+                DisplayAlert("Ops...", "Não deixe os campos em branco.", "Beleza!");
+            }           
+            else if (entryLogin.Text == "admin" ||
+                    entrySenha.Text == "admin")
+            {
+                Navigation.PushAsync(new MenuPrincipalCliente());
+            }
+        }
+
+        private void BtnCadastrarLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ConfrariApp/ConfrariApp/MenuPrincipalCliente.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConfrariApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuPrincipalCliente : ContentPage
+	{
+		public MenuPrincipalCliente ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnCardápio_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
